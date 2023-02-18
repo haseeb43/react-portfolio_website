4d9c1cde98df8a8ac90e204f6f6df7045bcf239f
Haseeb Ahmed Zia <hh03933@gmail.com>
2023-02-18 16:21:44 +0500

    all done:

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:111-111-1111">111-111-1111</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:haseebahmedzia143@gmail.com">
+            haseebahmedzia143@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialContainer>
+        <CompanyContainer>
+          <Slogan>Inovativing one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialIcons href="https://github.com/haseeb43">
+          <AiFillGithub size={"3rem"} />
+        </SocialIcons>
+
+        <SocialIcons href="https://www.linkedin.com/in/haseeb-569407181/">
+          <AiFillLinkedin size={"3rem"} />
+        </SocialIcons>
+
+        <SocialIcons href="https://instagram.com">
+          <AiFillInstagram size={"3rem"} />
+        </SocialIcons>
+      </SocialContainer>
+    </FooterWrapper>
   );
 };
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am a <strong>React.js</strong> Developer with knowledge in business
+        application development. Skilled in creating interactive user
+        interfaces, integrating user roles, and ensuring seamless user
+        experience. Committed to creating elegant, maintainable code and staying
+        current with web development trends.
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
